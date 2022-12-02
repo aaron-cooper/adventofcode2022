4a71b5d5032edcf42fa6b46a28099426ac698db0
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-02 14:10:34 -0500

    Day 1

---
+++ day1/p1.py
@@ -0,0 +1,14 @@
+
+
+inventories = []
+with open("input.txt", "r") as infile:
+    inventory = []
+    for line in infile:
+        line = line.strip()
+        if line == "":
+            inventories.append(sum(inventory))
+            inventory = []
+        else:
+            inventory.append(int(line))
+
+print(max(inventories))
