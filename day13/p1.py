84670847c80f061c24d2aca1df0eeb9d1853ed3b
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-18 16:35:04 -0500

    add day 13 part 1

--- day13/p1.py
+++ day13/p1.py
@@ -1,29 +1,37 @@
-def lex_line(line):
-    lexed = []
-    i = 0
-    while i < len(line):
-        if line[i] == '[':
-            lexed.append('[')
-            i += 1
-        elif line[i] == ']':
-            lexed.append(']')
-            i += 1
-        elif line[i] == ',':
-            i += 1
-        else: # digit
-            j = i
-            while line[j].isdigit():
-                j += 1
-            lexed.append(int(line[i:j]))
-            i = j
-    return lexed
+# sign is not mine
+def sign(x):
+  return int(x > 0) - int(x < 0)
+
+def compare_lines(line1, line2):
+    size = min(len(line1), len(line2))
+    for i in range (size):
+        if type(line1[i]) == type(line2[i]):
+            if type(line1[i]) == list:
+                if comparison := compare_lines(line1[i], line2[i]):
+                    return comparison
+            else: 
+                if sign(line1[i] - line2[i]): # if they're not equal
+                    return sign(line1[i] - line2[i])
+        elif type(line1[i]) == list:
+            if comparison := compare_lines(line1[i], [line2[i]]):
+                return comparison
+        else:
+            if comparison := compare_lines([line1[i]], line2[i]):
+                return comparison
+    return sign(len(line1) - len(line2))
 
 with open('input.txt', 'r') as file:
-    line = file.readline().strip()
-    lexed = lex_line(line)
-    for token in lexed:
-        print(token)
+    in_order = 0
+    i = 1
+    moreLines = True
+    while moreLines:
+        line1 = file.readline().strip()
+        line2 = file.readline().strip()
+        if line1 and line2:
+            in_order += i * (compare_lines(eval(line1), eval(line2)) != 1)
+        else:
+            moreLines = False
+        i += 1
+        file.readline() # skip a line
 
-
-
-
+print(in_order)
