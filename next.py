e72785886521375db4145945bed8657b355109e1
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-18 16:34:26 -0500

    Make next.py automatically copy p1 contents to p2

--- next.py
+++ next.py
@@ -2,6 +2,7 @@ from os import getcwd, path, chdir, mkdir
 from subprocess import run
 from sys import argv
 from fileinput import input
+from shutil import copy
 
 def update_debug_config(day, part):
     with input(files=('.vscode/launch.json'), inplace=True) as f:
@@ -34,7 +35,7 @@ if not path.exists(dayn(i - 1) + 'p2.py'): # move from p1 to p2
     i -= 1
     commit_curr_dir_contents(i, 1)
     dir = dayn(i)
-    touch(dir + 'p2.py')
+    copy(dir + 'p1.py', dir + 'p2.py')
     update_debug_config(i, 1)
     vscode(dir + 'p2.py')
 else:
