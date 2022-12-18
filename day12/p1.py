8815cf8c56ab8d596533c3696d25b4a269280754
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-18 01:47:27 -0500

    add day 12 part 1

---
+++ day12/p1.py
@@ -0,0 +1,90 @@
+#from helpers import MultiIndexSet
+from helpers.MultiIndexSet import MultiIndexSet
+
+class Node:
+    def __init__(self, elevation):
+        self.neighbours = set()
+        self.elevation = elevation
+
+    def try_add_neighbour(self, node):
+        if node.elevation - self.elevation <= 1:
+            self.neighbours.add(node)
+
+neighbour_ind_offsets = [
+    (-1, 0),
+    (0, -1),
+    (0, 1),
+    (1, 0)
+]
+
+class BoundChecker:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+
+    def __call__(self, i, j):
+        return 0 <= i and i < self.rows and 0 <= j and j < self.cols
+
+def dijkstra(graph, start, end):
+    to_visit = MultiIndexSet()
+    to_visit.add_index(lambda n: n.dist, 'distance')
+    for n in graph:
+        n.dist = 10000 # not infinity, close enough for our purposes
+        n.prev = None
+        to_visit.add(n)
+    to_visit.remove(start)
+    start.dist = 0
+    to_visit.add(start)
+
+    while len(to_visit):
+        curr = to_visit.pop_min('distance')
+        if curr == end:
+            break
+        for n in curr.neighbours:
+            if n not in to_visit:
+                continue
+            if curr.dist + 1 < n.dist:
+                to_visit.remove(n)
+                n.dist = curr.dist + 1
+                n.prev = curr
+                to_visit.add(n)
+    steps = 0
+    while curr.prev:
+        curr = curr.prev
+        steps += 1
+    return steps
+
+stoi = dict([(chr(97 + i), i) for i in range(26)])
+stoi['S'] = 0
+stoi['E'] = 25
+
+
+
+
+grid = []
+graph = set()
+with open ('input.txt', 'r') as file:
+    for line in file:
+        grid.append([])
+        for c in line.strip():
+            n = Node(stoi[c])
+            grid[len(grid) - 1].append(n)
+            graph.add(n)
+            if c == 'S':
+                start = n
+            elif c == 'E':
+                end = n
+
+rows = len(grid)
+cols = len(grid[0])
+checker = BoundChecker(rows, cols)
+
+for i in range(rows):
+    for j in range(cols):
+        for x, y in neighbour_ind_offsets:
+            (x, y) = (x + i, y + j)
+            if checker(x, y):
+                grid[i][j].try_add_neighbour(grid[x][y])
+
+print(dijkstra(graph, start, end))
+pass
