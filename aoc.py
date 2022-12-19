61b29250f3258aa942726d447b9cedffe9484ad0
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-18 22:30:31 -0500

    Update aoc.py

--- aoc.py
+++ aoc.py
@@ -2,19 +2,20 @@
 # so that I can separate some code into different files if I feel it might be
 # useful on other days.
 
-from argparse import ArgumentParser
+import argparse
 from importlib import import_module
 from shutil import copy
 
-parser = ArgumentParser(
+parser = argparse.ArgumentParser(
     description="This is a parent script for running my AoC solutions. Specify a day and part."
 )
 
 parser.add_argument("day", type=int)
 parser.add_argument("part", type=int)
+parser.add_argument("subpart", type=str, nargs='?', default='')
 
 args = parser.parse_args()
 
 copy(f'day{str(args.day).zfill(2)}/input.txt', 'input.txt')
 
-import_module(f"day{str(args.day).zfill(2)}.p{args.part}")
+import_module(f"day{str(args.day).zfill(2)}.p{args.part}{args.subpart}")
