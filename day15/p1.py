c6bde3ab4be3abca41ed8a67ab9630e591df73bc
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-24 19:23:27 -0500

    Finish p1.py

--- day15/p1.py
+++ day15/p1.py
@@ -2,6 +2,7 @@ import re
 from functools import cmp_to_key
 from queue import deque
 from sortedcontainers import SortedSet
+from helpers.bsearch import bsearch
 
 class Sensor:
     def __init__(self, x: int, y: int, point):
@@ -59,11 +60,10 @@ class Overlap:
     def __init__(self, sensor=None, interval=None, sensors=None):
         if sensor and not interval and not sensors:
             self.interval = sensor.coverage()
-            self.sensors = set()
-            self.sensors.add(sensor)
+            self.sensors = frozenset([sensor])
         elif interval and sensors and not sensor:
             self.interval = interval
-            self.sensors = sensors
+            self.sensors = frozenset(sensors)
         else:
             raise ValueError('specify sensor OR interval and sensors; not both')
 
@@ -82,33 +82,81 @@ class Overlap:
         new_sensors = self.sensors.union(other.sensors)
         return type(self)(interval=new_interval, sensors=new_sensors)
 
-    def lowest(self):
-        return self.interval[0]
+    def covered(self):
+        return self.interval[1] - self.interval[0] + 1
+
+    def __lt__(self, other):
+        if self.covered() % 2 == other.covered() % 2:
+            return sum(self.interval) // 2 < sum(other.interval) // 2
+        elif self.covered() % 2:
+            sum(self.interval) / 2 < sum(other.interval) // 2
+        else:
+            sum(self.interval) // 2 < sum(other.interval) / 2
 
 class UnionFinder:
-    def spots_covered(self, sensors):
-        overlap_key = lambda o: o.lowest()
-        overlapers = dict()
-        singleoverlaps = SortedSet([Overlap(sensor=s) for s in sensors], overlap_key)
-        overlaps = [set(singleoverlaps), set()]
 
-        # break this down into methods ...
-        for i, initial_overlap in enumerate(singleoverlaps):
-            overlapers[initial_overlap] = set()
-            for j in reversed(range(i)):
-                if new_overlap := initial_overlap.intersect(singleoverlaps[j]):
-                    overlaps[1].add(new_overlap)
-                    overlapers[initial_overlap].add(singleoverlaps[j])
-                else: # if we don't overlap with singleoverlaps[j], then we only
-                      # need to check overlaps which do overlap it
-                    for overlap in overlapers[singleoverlaps[j]]:
-                        if new_overlap := initial_overlap.intersect(overlap):
-                            overlaps[1].add(new_overlap)
-                            overlapers[initial_overlap].add(singleoverlaps[j])
-                    break
+    def __init__(self, sensors):
+        self.sensors = sensors
+
+    def spots_covered(self):
+        self.initial_overlaps = sorted(map(lambda s: Overlap(sensor=s), self.sensors))
+        self.construct_overlappers()
+        overlaps = [self.initial_overlaps]
+
+        i = 0
+        while (overlaps[i]):
+            overlaps.append(self.new_covered(overlaps[i]))
+            i+= 1
+
         covered = 0
-        for o in overlaps[0]:
-            covered += o.interval[1] - o.interval[0] + 1
-        for o in overlaps[1]:
-            covered -= o.interval[1] - o.interval[0] + 1
+        add_or_sub = 1
+        for l in overlaps[0:-1]:
+            for o in l:
+                covered += add_or_sub * (o.interval[1] - o.interval[0] + 1)
+            add_or_sub *= -1
+
         return covered
+
+    def new_covered(self, previous):
+        overlaps = []
+        already_added = set()
+        for i, overlap in enumerate(previous):
+            ind = bsearch(self.initial_overlaps, overlap)
+            ind = ~ind if ind < 0 else ind
+            for other in reversed(self.initial_overlaps[0:i]):
+                new_overlap = overlap.intersect(other)
+                if new_overlap and new_overlap.sensors != overlap.sensors and new_overlap.sensors not in already_added:
+                    overlaps.append(new_overlap)
+                    already_added.add(new_overlap.sensors)
+                else:
+                    for other in self.overlappers[other]:
+                        new_overlap = overlap.intersect(other)
+                        if new_overlap and new_overlap.sensors != overlap.sensors and new_overlap.sensors not in already_added:
+                            overlaps.append(new_overlap)
+                            already_added.add(new_overlap.sensors)
+        return overlaps
+
+    def construct_overlappers(self):
+        overlappers = dict()
+        for i, overlap in enumerate(self.initial_overlaps):
+            overlappers[overlap] = set()
+            for other in reversed(self.initial_overlaps[0:i]):
+                if overlap.intersect(other):
+                    overlappers[overlap].add(other)
+                else:
+                    for other in overlappers[other]:
+                        if overlap.intersect(other):
+                            overlappers[overlap].add(other)
+                break
+        self.overlappers = overlappers
+
+target = 2000000
+
+sensors, beacons = read_input()
+sensors = map(lambda s: s.flatten_y(target), sensors)
+sensors = list(filter(lambda s: s.r >= 0, sensors))
+beacons = list(filter(lambda b: b[1] == target, beacons))
+
+finder = UnionFinder(sensors)
+
+print(finder.spots_covered() - len(beacons))
