b89870576e31802667238420e4882f18fd465b1a
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2022-12-24 19:25:30 -0500

    Add script for running tests

---
+++ test.py
@@ -0,0 +1,18 @@
+# Script for running my AoC solutions. This lets me treat solutions as packages
+# so that I can separate some code into different files if I feel it might be
+# useful on other days.
+
+import argparse
+from shutil import copy
+
+parser = argparse.ArgumentParser(
+    description="This is a parent script for running my AoC solutions. Specify a day and part."
+)
+
+parser.add_argument("day", type=int)
+parser.add_argument("part", type=int)
+args = parser.parse_args()
+
+copy(f'day{str(args.day).zfill(2)}/input.txt', 'input.txt')
+
+exec(f"from day{str(args.day).zfill(2)} import tests{args.part}")
